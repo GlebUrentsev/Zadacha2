6dfefbab660a6e3859d50ea08f2f9efa7616224a
Gleb <Gleb-urencev@yandex.ru>
2019-05-30 12:40:49 +0500

    Добавьте файлы проекта.

---
+++ Zadacha1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace Zadacha1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {        
+                DateTime date = new DateTime(2008, 1, 1);
+                int n = int.Parse(Console.ReadLine());
+                date = date.AddDays(n);
+                if (date.Day < 10 && date.Month < 10)
+                {
+                    Console.WriteLine($"{date.DayOfWeek}, 0{date.Day}.0{date.Month}");
+                }
+                else if (date.Day < 10 && date.Month > 10)
+                {
+                    Console.WriteLine($"{date.DayOfWeek}, 0{date.Day}.{date.Month}");
+                }
+                else if (date.Day >= 10 && date.Month < 10)
+                {
+                    Console.WriteLine($"{date.DayOfWeek}, {date.Day}.0{date.Month}");
+                }
+                else if (date.Day >= 10 && date.Month>=10)
+                {
+                    Console.WriteLine($"{date.DayOfWeek}, {date.Day}.{date.Month}");
+                }           
+        }
+    }
+}
